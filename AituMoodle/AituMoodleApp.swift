7553c5c60f92cbc869842dd8e6793336ccd1c800
EvelinaAlexey <evelinaalexey@gmail.com>
2024-11-17 21:10:21 +0500

    Initial Commit

---
+++ AituMoodle/AituMoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AituMoodleApp.swift
+//  AituMoodle
+//
+//  Created by EvelinaAlexey on 17.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct AituMoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
